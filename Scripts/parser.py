8c99466de8b9bb8007a2ba4e404436a8d3a7c51a
JDowns412 <jmdz412@gmail.com>
2018-04-01 22:25:38 -0400

    Modified the parser script to fit new data. TODO: www.

--- Scripts/parser.py
+++ Scripts/parser.py
@@ -1,64 +1,100 @@
-import os, json, math, pprint
+# (on windows) usage: python3 parser.py [USE CASE]
+# example (windows) usage: python3 parser.py 3
 
-def parse(length):
-    # change to the directory where the Alexa lists are
-    os.chdir("../Data")
+import os, json, sys
+from copy import deepcopy
 
-    files = []
-    data = {}
-    domains = set()
-    results = {"sites" : []}
+def parseNew(data):
+
+    results = deepcopy(data)
 
     for file in os.listdir():
-        if (file.split(".")[-1] == "py" or file.split(".")[-1] == "json"):
-            # ignore the file, since we don't want to evaluate on a python file
-            pass
-        else: #it is a file we want to parse in alexa domains
-            # we build the results dictionary as we go, file by file
+        # if it's the cisco data
+        if (file == "top-1m.csv"):
+            with open(file, 'r') as f:
+                # read in the data from the entire file into this temporary list
+                for line in f:
+                    if (len(line) > 0):
+                        if (line.split(",")[1].rstrip() not in results):
+                            results[line.split(",")[1].rstrip()] = {"rank" : int(line.split(",")[0])}
+
+                # report progress
+                print("Parsed in top sites from Cisco")
+
+    return results
+
+
+
+def parseOld(data):
+
+    files = []
+    domains = set()
+    ignoreTypes = ["py", "json", "zip", "csv"]
+
+    results = deepcopy(data)
+
+    for file in os.listdir():
+        flag = True
+        for fType in ignoreTypes:
+            if (file.endswith(fType)):
+                # ignore the invalid file
+                flag = False
+            
+        if(flag):
+            #it is a file we want to parse
             files.append(file)
 
     # iterate over all of the files in the directory
     for file in files:
+        count = 0
         with open(file, 'r') as f:
             country = file.split("-")[1]
-            data[country] = []
             # read in the data from the entire file into this temporary list
             for line in f:
+                count += 1
                 if (len(line) > 0):
-                    data[country].append(line)
+                    if (line not in results):
+                        results[line.rstrip()] = {"country" : country, "rank" : count}
 
-    # now that data contains all of the top sites in one place (and we don't 
-    # have to open up files anymore), we can aggregate a psudo top 500 list.
-    # I have no way of knowing which sites are the most popular overall, but 
-    # I can take the top 500/[# of data files] from each file to get the top
-    # sites that I could guess. 
-    # TODO: Later we can apply proportions to these sites based on #users/country
-    # pprint.pprint(data)
+            # report progress
+            print("Parsed in top sites from %s" % country)
 
-    # print("\n")
+    return results
 
+
+
+def parse():
+    # change to the directory where the Alexa lists are
+    os.chdir("../Data")
+
+    results = {}
+
+    # USAGE CASES: #######################
+    # parse old alexa data
+    if (sys.argv[1] == "1"):
+        results = parseOld(results)
+
+    # parse new data (zone files and cisco data)
+    elif(sys.argv[1] == "2"):
+        results = parseNew(results)
+
+    # parse both
+    elif(sys.argv[1] == "3"):
+        results = parseOld(parseNew(results)) 
     
-    # we want to aggregate roughly this many sites together
-    goalLength = length
-    num = 0
-    while (len(results["sites"]) <= (goalLength + goalLength%len(data))):
-        # print(num)
-        # take off an even number of sites per country
-        for country in data:
-            # print(num)
-            if (data[country][num].split(".")[0] not in domains):
-                domains.add(data[country][num].split(".")[0])
-                results["sites"].append(data[country][num].strip())
-        num += 1
 
-    # check the length of the results "top" sites list
-    print("aggregated the \"top\" %d sites." % len(results["sites"]))
-
+    print("\nSaving results ...", end='')
+    
     # dump the results
-    with open("top%dEven.json" % goalLength, 'w') as fp:
+    with open("domains.json", 'w') as fp:
         json.dump(results, fp, indent=4)
+
+    print("... Done")
+
 
 
 if __name__ == "__main__":
-    goalLength = 5
-    parse(goalLength)
+    parse()
+
+
+# TODO: ask josh if he want's "www." in front of his hostnames or not
